42a77d4da7465255f272f892379e90c34fa8d424
UnnamedOrange <lycheng1215@sina.com>
2021-02-04 13:03:40 +0800

    Create the solution. Code page conversion.

---
+++ osu-kps/main.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) UnnamedOrange. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+#include <Windows.h>
+#undef min
+#undef max
+
+#include <utils/code_conv.hpp>
+
+int APIENTRY wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int)
+{
+
+	return 0;
+}
