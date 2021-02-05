c322db257973f4345109c1cc6699390df04ee3a4
UnnamedOrange <lycheng1215@sina.com>
2021-02-05 16:45:25 +0800

    Implement some basic functions of a window.

--- osu-kps/main.cpp
+++ osu-kps/main.cpp
@@ -13,6 +13,12 @@
 
 #include "integrated_kps.hpp"
 
+/* void OnMoving(HWND hwnd, RECT* pRect) */
+#define HANDLE_WM_MOVING(hwnd, wParam, lParam, fn)\
+	((fn)((hwnd), (RECT*)(lParam)), 0L)
+#define FORWARD_WM_MOVING(hwnd, pRect, fn)\
+	(void)(fn)((hwnd), WM_MOVING, 0L, (LPARAM)(pRect))
+
 class main_window : public window
 {
 	virtual INT_PTR WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) override
@@ -22,6 +28,25 @@ class main_window : public window
 			HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
 			HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
 
+			HANDLE_MSG(hwnd, WM_LBUTTONDOWN, OnLButtonDown);
+			HANDLE_MSG(hwnd, WM_NCLBUTTONDOWN, OnNCLButtonDown);
+			HANDLE_MSG(hwnd, WM_MOVING, OnMoving);
+
+		case WM_DPICHANGED:
+		{
+			// UpdateDpiDependentFontsAndResources();
+
+			RECT* const prcNewWindow = (RECT*)lParam;
+			SetWindowPos(hwnd,
+				NULL,
+				prcNewWindow->left,
+				prcNewWindow->top,
+				prcNewWindow->right - prcNewWindow->left,
+				prcNewWindow->bottom - prcNewWindow->top,
+				SWP_NOZORDER | SWP_NOACTIVATE);
+			break;
+		}
+
 		default:
 			return DefWindowProcW(hwnd, message, wParam, lParam);
 		}
@@ -30,6 +55,7 @@ class main_window : public window
 	BOOL OnCreate(HWND, LPCREATESTRUCT)
 	{
 		caption(L"osu!kps");
+		SetWindowLongW(hwnd, GWL_STYLE, WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_POPUP | WS_SYSMENU); // 无边框窗口。
 		SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 		return TRUE;
 	}
@@ -38,8 +64,68 @@ class main_window : public window
 		PostQuitMessage(0);
 	}
 
+	// 禁止窗口移出屏幕相关信息。
+	mutable POINT pMouse;
+	mutable RECT rectWnd;
+	void OnLButtonDown(HWND hwnd, BOOL fDoubleClick, int x, int y, UINT keyFlags)
+	{
+		if (fDoubleClick)
+		{
+			// TODO: 实现双击修改配置等功能。
+		}
+		else
+		{
+			PostMessageW(hwnd, WM_NCLBUTTONDOWN, HTCAPTION, MAKELPARAM(x, y));
+		}
+	}
+	void OnNCLButtonDown(HWND hwnd, BOOL fDoubleClick, int x, int y, UINT codeHitTest)
+	{
+		// 禁止窗口移出屏幕相关信息。
+		GetWindowRect(hwnd, &rectWnd);
+		GetCursorPos(&pMouse);
+		FORWARD_WM_NCLBUTTONDOWN(hwnd, fDoubleClick, x, y, codeHitTest, DefWindowProcW);
+		PostMessageW(hwnd, WM_LBUTTONUP, 0, 0);
+	}
+	void OnMoving(HWND hwnd, RECT* pRect)
+	{
+		RECT& r = *pRect;
+		POINT p;
+		GetCursorPos(&p);
+		RECT rWorkArea = work_area();
+		int nWidth = r.right - r.left;
+		int nHeight = r.bottom - r.top; // 获取窗口大小。
+		rWorkArea.right -= nWidth; // 将窗口可能出现的位置更改为窗口左上角的点可能出现的位置。
+		rWorkArea.bottom -= nHeight;
+
+		r.left = rectWnd.left + (p.x - pMouse.x); // 根据鼠标位置初步得出窗口左上角位置。
+		r.top = rectWnd.top + (p.y - pMouse.y);
+
+		POINT pCorner = { r.left, r.top }; // 窗口左上角的点。
+		if (pCorner.x < rWorkArea.left)
+		{
+			r.left = rWorkArea.left;
+		}
+		if (pCorner.y < rWorkArea.top)
+		{
+			r.top = rWorkArea.top;
+		}
+		if (pCorner.x > rWorkArea.right)
+		{
+			r.left = rWorkArea.right;
+		}
+		if (pCorner.y > rWorkArea.bottom)
+		{
+			r.top = rWorkArea.bottom;
+		}
+		r.right = r.left + nWidth; // 根据左上角位置和大小调整右下角。
+		r.bottom = r.top + nHeight;
+	}
+
 public:
 	kps::kps kps;
+
+public:
+	double scale{ 1 }; // 绘图时的额外比例因子。
 };
 
 int APIENTRY wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int)
