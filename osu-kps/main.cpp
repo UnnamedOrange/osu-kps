a2985a3c79dd7faa30e24650692c8caac4767b9b
UnnamedOrange <lycheng1215@sina.com>
2021-02-05 16:15:07 +0800

    timer_thread. Prepare for main window.

--- osu-kps/main.cpp
+++ osu-kps/main.cpp
@@ -1,17 +1,72 @@
 ﻿// Copyright (c) UnnamedOrange. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+#include <functional>
+
 #include <Windows.h>
 #undef min
 #undef max
 
 #include <utils/code_conv.hpp>
 #include <utils/window.hpp>
+#include <utils/timer_thread.hpp>
 
-#include "kps_calculator.hpp"
+#include "integrated_kps.hpp"
+
+class main_window : public window
+{
+	virtual INT_PTR WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) override
+	{
+		switch (message)
+		{
+			HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
+			HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
+
+		default:
+			return DefWindowProcW(hwnd, message, wParam, lParam);
+		}
+		return 0;
+	}
+	BOOL OnCreate(HWND, LPCREATESTRUCT)
+	{
+		caption(L"osu!kps");
+		SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+		return TRUE;
+	}
+	void OnDestroy(HWND)
+	{
+		PostQuitMessage(0);
+	}
+
+public:
+	kps::kps kps;
+};
 
 int APIENTRY wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int)
 {
+	// 设置高 DPI 支持。
+	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+	// 单例。
+	HANDLE hMutex = CreateMutexW(nullptr, FALSE, L"Global\\osu!kps-302F3137-DEA2-472F-9BC5-F078331E5576");
+	DWORD last_error = GetLastError();
+	if (last_error == ERROR_ALREADY_EXISTS ||
+		last_error == ERROR_ACCESS_DENIED && !hMutex)
+	{
+		if (hMutex)
+			CloseHandle(hMutex);
+		return 0;
+	}
+	else if (!hMutex)
+		return 1;
 
+	// 窗口与主循环。
+	{
+		main_window wnd;
+		wnd.create();
+		wnd.message_loop();
+	}
+
+	// 关闭单例中用到的句柄。
+	CloseHandle(hMutex);
 	return 0;
 }
