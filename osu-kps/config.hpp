2af24471cdcaab491db909b97076023bd50fc4b0
UnnamedOrange <lycheng1215@sina.com>
2021-02-12 21:47:28 +0800

    Implement configuring the keys in config.

--- osu-kps/config.hpp
+++ osu-kps/config.hpp
@@ -4,12 +4,15 @@
 #pragma once
 
 #include <utils/config_manager.hpp>
+#include <utils/keyboard_char.hpp>
 
 #include "integrated_kps.hpp"
 #include "keys_manager.hpp"
 
 class config : public config_manager
 {
+	keyboard_char kc;
+
 public:
 	/// <summary>
 	/// 将所有参数合法化。调用时首先假设所有参数的类型正确。
@@ -19,6 +22,14 @@ public:
 		scale(std::max(0.5, std::min(3.0, scale())));
 		button_count(std::max(1, std::min(static_cast<int>(keys_manager::max_key_count), button_count())));
 		kps_method(static_cast<kps::kps_implement_type>(std::max(0, std::min(0, static_cast<int>(kps_method())))));
+
+		for (int i = 1; i <= keys_manager::max_key_count; i++)
+			for (int j = 0; j < i; j++)
+			{
+				auto key = key_keyname(i, j);
+				if (!kc.is_supported(key))
+					(*this)[key] = u8"null";
+			}
 	}
 
 public:
@@ -74,5 +85,20 @@ public:
 	void kps_method(kps::kps_implement_type type)
 	{
 		(*this)[u8"kps.method"] = static_cast<int>(type);
+	}
+
+	static std::u8string key_keyname(int total, int which)
+	{
+		using namespace std::literals;
+		return reinterpret_cast<const char8_t*>(
+			("keys." + std::to_string(total) + "k.k" + std::to_string(which)).c_str());
+	}
+	int key_map(int total, int which) const
+	{
+		return kc.to_int(std::get<std::u8string>(get_value(key_keyname(total, which))));
+	}
+	void key_map(int total, int which, int key)
+	{
+		(*this)[key_keyname(total, which)] = reinterpret_cast<const char*>(kc.to_full(key).c_str());
 	}
 };
