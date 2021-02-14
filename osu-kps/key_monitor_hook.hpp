9d2a37357d92aa37dae206c1d0d89d99ebd3b023
UnnamedOrange <lycheng1215@sina.com>
2021-02-14 15:18:39 +0800

    Implement hook.

--- osu-kps/key_monitor_hook.hpp
+++ osu-kps/key_monitor_hook.hpp
@@ -7,6 +7,8 @@
 #include <thread>
 #include <semaphore>
 
+#include <utils/hook.hpp>
+
 #include <Windows.h>
 #undef min
 #undef max
@@ -16,7 +18,7 @@
 namespace kps
 {
 	/// <summary>
-	/// 使用钩子实现的按键监视器。同一线程只能创建一个这样的按键监视器，但考虑到只会有一个线程创建该对象，因此不保证线程安全。没有测试在两个线程上存在两个该实例是否能正确运行。
+	/// 使用钩子实现的按键监视器。
 	/// </summary>
 	class key_monitor_hook : public key_monitor_base
 	{
@@ -25,78 +27,44 @@ namespace kps
 		using key_monitor_base::_on_llkey_up;
 
 	private:
-		HHOOK hKeybd{};
-		HHOOK hMouse{};
-		// 线程 id 到线程对应按键监视器实例的映射。因此同一线程只能创建一个基于钩子的按键监视器。
-		inline static std::unordered_map<std::thread::id, key_monitor_hook*> id2instance;
-		static LRESULT CALLBACK VirtualKeybdProc(int nCode, WPARAM wParam, LPARAM lParam)
+		keyboard_hook k_hook;
+		bool keyboard_proc(bool down, int vk, std::chrono::steady_clock::time_point time)
 		{
-			return id2instance[std::this_thread::get_id()]->KeybdProc(nCode, wParam, lParam);
+			UNREFERENCED_PARAMETER(time);
+			if (down)
+				_on_llkey_down(vk, time);
+			else
+				_on_llkey_up(vk);
+			return false;
 		}
-		LRESULT KeybdProc(int nCode, WPARAM wParam, LPARAM lParam)
+		mouse_hook m_hook;
+		bool mouse_proc(UINT message, PMSLLHOOKSTRUCT ms, std::chrono::steady_clock::time_point time)
 		{
-			if (nCode < 0)
-				return CallNextHookEx(hKeybd, nCode, wParam, lParam);
-
-			auto ks = PKBDLLHOOKSTRUCT(lParam);
-			if (nCode == HC_ACTION)
-			{
-				if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
-					_on_llkey_down(ks->vkCode, clock::now());
-				else if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP)
-					_on_llkey_up(ks->vkCode);
-			}
-			return CallNextHookEx(hKeybd, nCode, wParam, lParam);
-		}
-		static LRESULT CALLBACK VirtualMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
-		{
-			return id2instance[std::this_thread::get_id()]->MouseProc(nCode, wParam, lParam);
-		}
-		LRESULT MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
-		{
-			if (nCode < 0)
-				return CallNextHookEx(hMouse, nCode, wParam, lParam);
-
-			if (nCode == HC_ACTION)
-			{
-				if (wParam == WM_LBUTTONDOWN)
-					_on_llkey_down(VK_LBUTTON, clock::now());
-				else if (wParam == WM_LBUTTONUP)
-					_on_llkey_up(VK_LBUTTON);
-				else if (wParam == WM_RBUTTONDOWN)
-					_on_llkey_down(VK_RBUTTON, clock::now());
-				else if (wParam == WM_RBUTTONUP)
-					_on_llkey_up(VK_RBUTTON);
-			}
-			return CallNextHookEx(hMouse, nCode, wParam, lParam);
+			UNREFERENCED_PARAMETER(ms);
+			UNREFERENCED_PARAMETER(time);
+			if (message == WM_LBUTTONDOWN)
+				_on_llkey_down(VK_LBUTTON, clock::now());
+			else if (message == WM_LBUTTONUP)
+				_on_llkey_up(VK_LBUTTON);
+			else if (message == WM_RBUTTONDOWN)
+				_on_llkey_down(VK_RBUTTON, clock::now());
+			else if (message == WM_RBUTTONUP)
+				_on_llkey_up(VK_RBUTTON);
+			return false;
 		}
 	public:
 		key_monitor_hook()
 		{
-			if (id2instance.count(std::this_thread::get_id()))
-				throw std::runtime_error("there has been another instance in this thread.");
-			hKeybd = SetWindowsHookExW(WH_KEYBOARD_LL, &key_monitor_hook::VirtualKeybdProc,
-				GetModuleHandleW(nullptr), NULL);
-			if (!hKeybd)
-				throw std::runtime_error("fail to SetWindowsHookExW(WH_KEYBOARD_LL, ...).");
-			hMouse = SetWindowsHookExW(WH_MOUSE_LL, &key_monitor_hook::VirtualMouseProc,
-				GetModuleHandleW(nullptr), NULL);
-			if (!hMouse)
-			{
-				UnhookWindowsHookEx(hKeybd);
-				throw std::runtime_error("fail to SetWindowsHookExW(WH_MOUSE_LL, ...).");
-			}
-			id2instance[std::this_thread::get_id()] = this;
+			k_hook.set_callback(std::bind(&key_monitor_hook::keyboard_proc,
+				this,
+				std::placeholders::_1,
+				std::placeholders::_2,
+				std::placeholders::_3));
+			m_hook.set_callback(std::bind(&key_monitor_hook::mouse_proc,
+				this,
+				std::placeholders::_1,
+				std::placeholders::_2,
+				std::placeholders::_3));
 		}
-		~key_monitor_hook()
-		{
-			id2instance.erase(std::this_thread::get_id());
-			UnhookWindowsHookEx(hMouse);
-			UnhookWindowsHookEx(hKeybd);
-		}
-		key_monitor_hook(const key_monitor_hook&) = delete;
-		key_monitor_hook(key_monitor_hook&&) = delete;
-		key_monitor_hook& operator=(const key_monitor_hook&) = delete;
-		key_monitor_hook& operator=(key_monitor_hook&&) = delete;
 	};
 }
---
+++ utils/hook.hpp
@@ -0,0 +1,255 @@
+﻿// Copyright (c) UnnamedOrange. Licensed under the MIT Licence.
+// See the LICENSE file in the repository root for full licence text.
+
+// This header is designed to be used in Windows®.
+
+#pragma once
+
+#include <unordered_map>
+#include <functional>
+#include <thread>
+#include <semaphore>
+#include <chrono>
+
+#include <Windows.h>
+#undef min
+#undef max
+
+class _hook_base
+{
+private:
+	std::binary_semaphore bs{ 0 };
+	std::binary_semaphore bs_descendent{ 0 };
+	std::thread hook_thread;
+	void thread_proc()
+	{
+		MSG msg;
+		// Ask the system to create a message queue.
+		PeekMessageW(&msg, nullptr, 0, 0, PM_NOREMOVE);
+		bs.release();
+		bs_descendent.acquire();
+		_do_before_message_loop();
+		while (GetMessageW(&msg, nullptr, 0, 0))
+		{
+			TranslateMessage(&msg);
+			DispatchMessageW(&msg);
+		}
+	}
+protected:
+	/// <summary>
+	/// Get id of the thread the hook runs on. The thread is guaranteed to be created after the base class is constructed.
+	/// </summary>
+	/// <returns></returns>
+	DWORD get_thread_id() const
+	{
+		auto id = hook_thread.get_id();
+		return *reinterpret_cast<const DWORD*>(&id);
+	}
+
+private:
+	/// <summary>
+	/// This function will run on an individual thread.
+	/// </summary>
+	virtual void _do_before_message_loop() = 0;
+protected:
+	/// <summary>
+	/// You must call this function after critical variables of the descendent class are constructed.
+	/// After calling it, _do_before_message_loop will be called on an individual thread.
+	/// </summary>
+	void _constructed()
+	{
+		bs_descendent.release();
+	}
+
+public:
+	_hook_base() :
+		hook_thread(&_hook_base::thread_proc, this)
+	{
+		bs.acquire(); // Wait until the message queue is created.
+	}
+	~_hook_base()
+	{
+		PostThreadMessageW(get_thread_id(), WM_QUIT, 0, 0);
+		hook_thread.join();
+	}
+	_hook_base(const _hook_base&) = delete;
+	_hook_base(_hook_base&&) = delete;
+	_hook_base& operator=(const _hook_base&) = delete;
+	_hook_base& operator=(_hook_base&&) = delete;
+};
+
+/// <summary>
+/// The keyboard hook. Call set_callback to register a callback (see keyboard_hook::callback_t). The callback function will run on an individual thread.
+/// </summary>
+class keyboard_hook final : public _hook_base
+{
+private:
+	inline static std::binary_semaphore bs_map{ 1 };
+	inline static std::unordered_map<DWORD, keyboard_hook*> id2instance;
+	static LRESULT CALLBACK virtual_proc(int nCode, WPARAM wParam, LPARAM lParam)
+	{
+		auto now = std::chrono::steady_clock::now();
+
+		static thread_local keyboard_hook* instance;
+		if (!instance)
+		{
+			bs_map.acquire();
+			instance = id2instance[GetCurrentThreadId()];
+			bs_map.release();
+		}
+
+		if (nCode < 0)
+			return CallNextHookEx(instance->hKeybd, nCode, wParam, lParam);
+		if (nCode == HC_ACTION)
+		{
+			auto ks = PKBDLLHOOKSTRUCT(lParam);
+			bool skip = false;
+			instance->bs_callback.acquire();
+			if (instance->callback)
+			{
+				skip = instance->callback(
+					wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN,
+					ks->vkCode,
+					now
+				);
+			}
+			instance->bs_callback.release();
+
+			if (skip)
+				return 1;
+		}
+		return CallNextHookEx(instance->hKeybd, nCode, wParam, lParam);
+	}
+private:
+	virtual void _do_before_message_loop() override
+	{
+		hKeybd = SetWindowsHookExW(WH_KEYBOARD_LL, virtual_proc, nullptr, NULL);
+	}
+private:
+	HHOOK hKeybd{};
+public:
+	keyboard_hook()
+	{
+		bs_map.acquire();
+		id2instance[get_thread_id()] = this;
+		bs_map.release();
+		_constructed();
+	}
+	~keyboard_hook()
+	{
+		UnhookWindowsHookEx(hKeybd);
+		bs_map.acquire();
+		id2instance.erase(get_thread_id());
+		bs_map.release();
+	}
+
+public:
+	/// <summary>
+	/// Returning true means to skip the key message.
+	/// </summary>
+	using callback_t = std::function<bool(bool down, int vk, std::chrono::steady_clock::time_point time)>;
+private:
+	std::binary_semaphore bs_callback{ 1 };
+	callback_t callback;
+public:
+	void set_callback(callback_t func)
+	{
+		bs_callback.acquire();
+		callback = func;
+		bs_callback.release();
+	}
+	void reset_callback()
+	{
+		bs_callback.acquire();
+		callback = callback_t();
+		bs_callback.release();
+	}
+};
+
+/// <summary>
+/// The mouse hook. Call set_callback to register a callback (see mouse_hook::callback_t). The callback function will run on an individual thread.
+/// </summary>
+class mouse_hook final : public _hook_base
+{
+private:
+	inline static std::binary_semaphore bs_map{ 1 };
+	inline static std::unordered_map<DWORD, mouse_hook*> id2instance;
+	static LRESULT CALLBACK virtual_proc(int nCode, WPARAM wParam, LPARAM lParam)
+	{
+		auto now = std::chrono::steady_clock::now();
+
+		static thread_local mouse_hook* instance;
+		if (!instance)
+		{
+			bs_map.acquire();
+			instance = id2instance[GetCurrentThreadId()];
+			bs_map.release();
+		}
+
+		if (nCode < 0)
+			return CallNextHookEx(instance->hMouse, nCode, wParam, lParam);
+		if (nCode == HC_ACTION)
+		{
+			auto ms = PMSLLHOOKSTRUCT(lParam);
+			bool skip = false;
+			instance->bs_callback.acquire();
+			if (instance->callback)
+			{
+				skip = instance->callback(
+					wParam,
+					ms,
+					now
+				);
+			}
+			instance->bs_callback.release();
+
+			if (skip)
+				return 1;
+		}
+		return CallNextHookEx(instance->hMouse, nCode, wParam, lParam);
+	}
+private:
+	virtual void _do_before_message_loop() override
+	{
+		hMouse = SetWindowsHookExW(WH_MOUSE_LL, virtual_proc, nullptr, NULL);
+	}
+private:
+	HHOOK hMouse{};
+public:
+	mouse_hook()
+	{
+		bs_map.acquire();
+		id2instance[get_thread_id()] = this;
+		bs_map.release();
+		_constructed();
+	}
+	~mouse_hook()
+	{
+		UnhookWindowsHookEx(hMouse);
+		bs_map.acquire();
+		id2instance.erase(get_thread_id());
+		bs_map.release();
+	}
+
+public:
+	/// <summary>
+	/// Returning true means to skip the key message.
+	/// </summary>
+	using callback_t = std::function<bool(UINT message, PMSLLHOOKSTRUCT ms, std::chrono::steady_clock::time_point time)>;
+private:
+	std::binary_semaphore bs_callback{ 1 };
+	callback_t callback;
+public:
+	void set_callback(callback_t func)
+	{
+		bs_callback.acquire();
+		callback = func;
+		bs_callback.release();
+	}
+	void reset_callback()
+	{
+		bs_callback.acquire();
+		callback = callback_t();
+		bs_callback.release();
+	}
+};
